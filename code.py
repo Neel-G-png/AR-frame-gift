618448fdb01747062d3d613aefb2c580f8da7e2d
Neel-G-png <neel701@gmail.com>
2021-08-24 16:10:34 +0530

    first commit

--- code.py
+++ code.py
@@ -130,4 +130,5 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
