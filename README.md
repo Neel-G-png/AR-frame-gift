f0862a2e206607ed3b83cbf1e87f1265e53a2dc7
Neel-G-png <neel701@gmail.com>
2021-08-26 13:13:54 +0530

    commit for public use

---
+++ README.md
@@ -0,0 +1,11 @@
+# Augmented reality Photo frame * Incredible as a birthday gift ;) *
+
+## How to use
+1. Buy a photo frame 
+2. Print Whatever photo you want
+3. Print the markers from marker folder.
+4. Attach markers on the corners of the photo as mentioned in the marker name (names end with the position. Ex "tl" = top left)
+5. Run flask severer (ocvgift.py)
+
+## 6. MOST IMPORTANT ONE 
+Give credit to Neel gandhi. 
--- ocvgift.py
+++ ocvgift.py
@@ -1,38 +1,18 @@
 from flask import Flask, render_template, Response, request
-from cv2 import cv2
+import cv2
 import datetime, time
 import os, sys
 import numpy as np
 
-
-global capture,rec_frame, grey, switch, neg, face, rec, out 
-capture=0
-grey=0
-neg=0
-face=0
-switch=1
-rec=0
-
-#make shots directory to save pics
-# try:
-#     os.mkdir('./shots')
-# except OSError as error:
-#     pass
-
-#Load pretrained face detection model    
-# net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
-
 #instatiate flask app  
 app = Flask(__name__)
 
-# im_src = cv2.imread("new_scenery.jpg")
 camera = cv2.VideoCapture(0)
-
 
 def gen_frames():  # generate frame by frame from camera
     global out, capture,rec_frame
     camera = cv2.VideoCapture(0)
-    cap2 = cv2.VideoCapture("videoplayback.mp4")
+    cap2 = cv2.VideoCapture("hbd.mp4")
     counter = 0
     while True:
         success, frame = camera.read()
@@ -45,6 +25,11 @@ def gen_frames():  # generate frame by frame from camera
             parameters =  cv2.aruco.DetectorParameters_create()
             try:
                 markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
+                # if not markerIds:
+                #     ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                #     frame = buffer.tobytes()
+                #     yield (b'--frame\r\n'
+                #         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
                 
                 if len(markerIds) == 4:
                     counter = 0
@@ -108,7 +93,7 @@ def gen_frames():  # generate frame by frame from camera
                         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
                 
             except Exception as e:
-                # print(e)
+                print(e)
                 time.sleep(1)
                 counter+=1
                 if counter>=10:
